4b91f7fb064f70fc16577e739672fc6dc7e6d46d
rmtrout42 <rmtrout42@outlook.com>
2020-05-18 10:56:04 -0400

    added four basic operations

--- calculator.py
+++ calculator.py
@@ -2,3 +2,26 @@
 a = input("Enter the first number: ")
 b = input("Enter the second number: ")
 print("The numbers you entered are: {} and {}".format(a, b))
+
+c = input("Enter a command: ")
+
+if c == 'a':
+    print("Add")
+    ans = int(a) + int(b)
+    print('{} + {} = {}'.format(a, b, ans))
+elif c == 's':
+    print("Subtract")
+    ans = int(a) - int(b)
+    print('{} - {} = {}'.format(a, b, ans))
+elif c == 'm':
+    print("Multiply")
+    ans = int(a) * int(b)
+    print('{} * {} = {}'.format(a, b, ans))
+elif c == 'd':
+    print("Divide")
+    ans = int(a) / int(b)
+    print('{} / {} = {}'.format(a, b, ans))
+else:
+    print("{} is not a valid command".format(c))
+
+print('Finished')
